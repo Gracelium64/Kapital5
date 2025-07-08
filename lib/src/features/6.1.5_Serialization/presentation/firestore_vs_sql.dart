1845c6b6fc93f1367e2e280a815ea3fb196c40d3
Grace <grace6424@icloud.com>
2025-07-08 14:59:01 +0200

    6.1.5 ----- Refactor FirestoreVsSql widget layout and improve input field presentation

--- lib/src/features/6.1.5_Serialization/presentation/firestore_vs_sql.dart
+++ lib/src/features/6.1.5_Serialization/presentation/firestore_vs_sql.dart
@@ -2,6 +2,7 @@ import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:kapital_5/src/features/6.1.4_firestore_101/presentation/widgets/my_text_form_field.dart';
 import 'package:kapital_5/src/features/6.1.5_Serialization/domain/serialization.dart';
+import 'package:kapital_5/src/theme/palette.dart';
 
 class FirestoreVsSql extends StatefulWidget {
   const FirestoreVsSql({super.key});
@@ -45,81 +46,132 @@ class _FirestoreVsSqlState extends State<FirestoreVsSql> {
         ),
         title: Text('Firestore 101'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            MyTextFormField(
-              userInput: dailyUsersInput,
-              hintText: 'Number of users',
-              helperText: 'Number of users',
-            ),
-            MyTextFormField(
-              userInput: userDailyReadsInput,
-              hintText: 'Amount of reads per User',
-              helperText: 'Amount of reads per User',
-            ),
-            MyTextFormField(
-              userInput: userDailyWritesInput,
-              hintText: 'Amount of writes per User',
-              helperText: 'Amount of writes per User',
-            ),
-            MyTextFormField(
-              userInput: userDailyDeletesInput,
-              hintText: 'Amount of deletes per User',
-              helperText: 'Amount of deletes per User',
-            ),
-            MyTextFormField(
-              userInput: dailyTrafficMBInput,
-              hintText: 'Traffic in MB daily',
-              helperText: 'Traffic in MB daily',
-            ),
-            MyTextFormField(
-              userInput: dailyDataMBInput,
-              hintText: 'Data in MB daily',
-              helperText: 'Data in MB daily',
-            ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            spacing: 8,
+            children: [
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 50,
+                        width: 200,
 
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  final int dailyUsers = int.parse(dailyUsersInput.text);
-                  final int dailyReads = int.parse(userDailyReadsInput.text);
-                  final int dailyWrites = int.parse(userDailyWritesInput.text);
-                  final int dailyDeletes = int.parse(
-                    userDailyDeletesInput.text,
-                  );
-                  final int trafficMB = int.parse(dailyTrafficMBInput.text);
-                  final int dataMB = int.parse(dailyDataMBInput.text);
+                        child: MyTextFormField(
+                          userInput: dailyUsersInput,
+                          hintText: 'Number of users',
+                          helperText: 'Number of users',
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                        width: 200,
 
-                  final results = await serialization(
-                    dailyUsers,
-                    dailyReads,
-                    dailyWrites,
-                    dailyDeletes,
-                    trafficMB,
-                    dataMB,
-                  );
+                        child: MyTextFormField(
+                          userInput: userDailyReadsInput,
+                          hintText: 'Amount of reads per User',
+                          helperText: 'Amount of reads per User',
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                        width: 200,
 
-                  setState(() {
-                    outputLines = results;
-                  });
-                } catch (e) {
-                  setState(() {
-                    outputLines = [
-                      'Error: All fields must have a valid number',
-                    ];
-                  });
-                }
-              },
-              child: Text('FireStore vs SQL Case Study'),
-            ),
-            SizedBox(height: 20),
-            Text(
-              outputLines.join(
-                '\n',
+                        child: MyTextFormField(
+                          userInput: userDailyWritesInput,
+                          hintText: 'Amount of writes per User',
+                          helperText: 'Amount of writes per User',
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                        width: 200,
+
+                        child: MyTextFormField(
+                          userInput: userDailyDeletesInput,
+                          hintText: 'Amount of deletes per User',
+                          helperText: 'Amount of deletes per User',
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                        width: 200,
+
+                        child: MyTextFormField(
+                          userInput: dailyTrafficMBInput,
+                          hintText: 'Traffic in MB daily',
+                          helperText: 'Traffic in MB daily',
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                        width: 200,
+
+                        child: MyTextFormField(
+                          userInput: dailyDataMBInput,
+                          hintText: 'Data in MB daily',
+                          helperText: 'Data in MB daily',
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(
+                    width: 175,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: Text(
+                        outputLines.join(
+                          '\n',
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    final int dailyUsers = int.parse(dailyUsersInput.text);
+                    final int dailyReads = int.parse(
+                      userDailyReadsInput.text,
+                    );
+                    final int dailyWrites = int.parse(
+                      userDailyWritesInput.text,
+                    );
+                    final int dailyDeletes = int.parse(
+                      userDailyDeletesInput.text,
+                    );
+                    final int trafficMB = int.parse(dailyTrafficMBInput.text);
+                    final int dataMB = int.parse(dailyDataMBInput.text);
+
+                    final results = await serialization(
+                      dailyUsers,
+                      dailyReads,
+                      dailyWrites,
+                      dailyDeletes,
+                      trafficMB,
+                      dataMB,
+                    );
+
+                    setState(() {
+                      outputLines = results;
+                    });
+                  } catch (e) {
+                    setState(() {
+                      outputLines = [
+                        'Error: All fields must have a valid number',
+                      ];
+                    });
+                  }
+                },
+                child: Text('FireStore vs SQL Case Study'),
+              ),
+            ],
+          ),
         ),
       ),
     );
